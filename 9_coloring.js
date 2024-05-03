ca140f089628f0e477cfc82640c4a26585b5a928
AniMate002 <kirill1974111@gmail.com>
2024-05-03 11:59:23 +0200

    Update 9_coloring.js

--- 9_coloring.js
+++ 9_coloring.js
@@ -1,4 +1,3 @@
-// Define the graph
 const graph = {
     a: ['b', 'c'],
     b: ['a', 'd', 'e'],
@@ -12,19 +11,16 @@ const graph = {
     j: ['i']
 };
 
-// Function to color the graph
 function coloring(graph) {
     const colors = ["blue", "green", "red", "orange", "pink", "purple"];
     const coloredNodes = {};
 
-    // Iterate through each node in descending order of degrees
     const sortedNodes = Object.keys(graph).sort((a, b) => graph[b].length - graph[a].length);
     
     sortedNodes.forEach(node => {
         const neighbors = graph[node];
         const usedColors = new Set(neighbors.map(neighbor => coloredNodes[neighbor]));
 
-        // Find the first available color for the node
         let color;
         for (let c of colors) {
             if (!usedColors.has(c)) {
@@ -33,14 +29,11 @@ function coloring(graph) {
             }
         }
 
-        // Assign the color to the node
         coloredNodes[node] = color;
     });
 
-    // Output the coloring result
     console.log("Node Colors:", coloredNodes);
 
 }
 
-// Call the coloring function
 coloring(graph);
